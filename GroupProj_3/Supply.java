e0ffa907d1ce11d62c13ed425082b48e97c5e3ba
Himaalishaq <himaal.ishaq215@gmail.com>
2026-02-25 14:14:17 -0700

    Add files via upload

---
+++ GroupProj_3/Supply.java
@@ -0,0 +1,40 @@
+package edu.ucalgary.oop;
+
+public class Supply {
+
+    private String type;
+    private int quantity;
+
+    public Supply(String type, int quantity) {
+        setType(type);
+        setQuantity(quantity);
+    }
+
+    public void setType(String type) {
+        if (type == null) {
+            throw new IllegalArgumentException();
+        }
+        this.type = type;
+    }
+
+    public void setQuantity(int quantity) {
+        if (quantity < 0) {
+            throw new IllegalArgumentException();
+        }
+        this.quantity = quantity;
+    }
+    
+    public String getType() {
+        return this.type;
+    }
+    
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+
+
+
+
+
+}
