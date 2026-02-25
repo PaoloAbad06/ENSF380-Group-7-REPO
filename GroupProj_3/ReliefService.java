806afadfdecb1ec7ba19613cbbd4210d2cf6a718
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-02-25 16:43:39 -0700

    Fixed returning null value with last name

--- GroupProj_3/ReliefService.java
+++ GroupProj_3/ReliefService.java
@@ -75,7 +75,8 @@ public class ReliefService {
     public String getLogDetails()
     {
     	return "Inquirer: " + inquirer.getFirstName() + ", Missing Person: " + missingPerson.getFirstName() + " " 
-    	+ missingPerson.getLastName() + ", Date of Inquiry: " + dateOfInquiry + ", Info Provided: " 
-    	+ infoProvided + ", Last Known Location: " + lastKnownLocation.getName();
+    	+ ", Date of Inquiry: " + dateOfInquiry + ", Info Provided: " + infoProvided + ", Last Known Location: " 
+        + lastKnownLocation.getName();
     }
 }
+
