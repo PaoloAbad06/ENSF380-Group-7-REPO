0758b2c8f0bf37eb92ec6e30230fbdfdbad86909
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-02-25 17:20:11 -0700

    Fix formatting issues in ReliefService.java

--- GroupProj_3/ReliefService.java
+++ GroupProj_3/ReliefService.java
@@ -74,9 +74,10 @@ public class ReliefService {
     
     public String getLogDetails()
     {
-    	return "Inquirer: " + inquirer.getFirstName() + ", Missing Person: " + missingPerson.getFirstName() + " " 
+    	return "Inquirer: " + inquirer.getFirstName() + ", Missing Person: " + missingPerson.getFirstName() 
     	+ ", Date of Inquiry: " + dateOfInquiry + ", Info Provided: " + infoProvided + ", Last Known Location: " 
         + lastKnownLocation.getName();
     }
 }
+
 
